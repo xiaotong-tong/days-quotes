6c388b63fe1beffc80fa05039024992216098f2f
小恸恸 <wtt150249@gmail.com>
2024-05-14 21:17:34 +0800

    feat: 添加获取个数的接口

--- src/index.cjs
+++ src/index.cjs
@@ -3,3 +3,5 @@ const marchOf2024 = require("./lib/2024.json");
 exports.list = [].concat(marchOf2024);
 
 exports.itemByKey = (key) => exports.list.find((item) => item.key === key);
+
+exports.quotesCount = exports.list.length;
--- src/index.mjs
+++ src/index.mjs
@@ -3,3 +3,5 @@ import marchOf2024 from "./lib/2024.json" assert { type: "json" };
 export const list = [].concat(marchOf2024);
 
 export const itemByKey = (key) => list.find((item) => item.key === key);
+
+export const quotesCount = list.length;
