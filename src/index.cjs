b7540071018e4b95f4c9759b84b94589a519112a
小恸恸 <wtt150249@gmail.com>
2024-07-10 21:47:05 +0800

    fix: 修正路径错误

--- src/index.cjs
+++ src/index.cjs
@@ -1,6 +1,6 @@
 const marchOf1To100 = require("./lib/1-100.json");
 
-const poetryOf1To100 = require("./poetryLib/lib/1-100.json");
+const poetryOf1To100 = require("./poetryLib/1-100.json");
 
 // 每日一句日语学习
 exports.list = [].concat(marchOf1To100);
